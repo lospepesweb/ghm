4f2be21d71b6c0cf316c2960d2690b0458a02734
Petro232 <andpetronio@outlook.com>
2017-11-17 12:09:44 -0300

    script cruces spans del nav

--- js/scripts.js
+++ js/scripts.js
@@ -38,13 +38,23 @@ $('#nav-icon').click(function(){
 	$(this).toggleClass('open');
 
 	if($('.navClose-link').children().hasClass("open")){
-		$('.navClose-link').children().toggleClass("open");
+		$('.navClose-link').children().removeClass("open");
 	}
 
 })
 
-$('.navClose-link').click(function(){
-	$(this).children().toggleClass("open")
+$('.navClose-link').click(function(e){
+	
+	var id = e.target.id;
+
+	$('.navClose-link').children().removeClass("open");
+	
+	if(!$('#'+id).parent().hasClass('show')){
+		$('#'+id).children().addClass('open');
+	} else {
+		$('#'+id).children().removeClass('open');
+	}
+
 })
 
 

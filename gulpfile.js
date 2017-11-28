071e2c81e09fd9ca3fc45e0e1fb2cacad02926e0
Lucho Peñafiel <lucianopenafiel@gmail.com>
2017-11-27 21:46:51 -0300

    ordenando los archivos en la carpeta dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var minifycss = require('gulp-minify-css');
 
 
 gulp.task('minify', function(){
-	return gulp.src('css/estilos.css')
+	return gulp.src('dist/css/estilos.css')
 		.pipe(minifycss())
-		.pipe(gulp.dest('css'));
+		.pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function(){
-	gulp.watch('css/estilos.css', ['minify']);
+	gulp.watch('dist/css/estilos.css', ['minify']);
 });
 
 

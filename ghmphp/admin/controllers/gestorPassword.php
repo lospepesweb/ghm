bb30ac304722f42e8500c181c0c1edf1c1f32792
Petro232 <andpetronio@outlook.com>
2017-12-17 21:15:49 -0300

    corregido error ordenar slider, agregado favicon al backend

--- ghmphp/admin/controllers/gestorPassword.php
+++ ghmphp/admin/controllers/gestorPassword.php
@@ -1,6 +1,6 @@
 <?php
 
-class GestorPassword{
+class GestorPassword {
 
 	public function cambiarPasswordController(){
 		
@@ -74,66 +74,3 @@ class GestorPassword{
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-				// $encriptar = crypt($_POST['password'], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
-			
-// 				$datosController = array('usuario'=>$_POST['usuarioLogin'],
-// 				                         'password'=>$_POST['passwordLogin']);
-										 
-// 				$respuesta = LoginM::LoginModelo($datosController, 'usuarios');
-				
-// 				$intentos = $respuesta['intentos'];
-// 				$usuarioActual = $_POST['usuarioLogin'];
-// 				$maximoIntentos = 2;
-				
-// 				if($intentos < $maximoIntentos){
-
-// 					if($respuesta['usuario'] == $_POST['usuarioLogin'] && 
-// 					   $respuesta['password'] == $_POST['passwordLogin']){
-						
-// 						$intentos = 0;
-
-// 						$datosController = array('usuarioActual'=>$usuarioActual, 
-// 											     'actualizarIntentos'=>$intentos);
-
-// 						$respuestaActualizarIntentos = LoginM::intentosModelo($datosController, 'usuarios');
-
-// 						session_start();
-
-// 						$_SESSION['validar'] = true;
-// 						$_SESSION['usuario'] = $respuesta['usuario'];
-						
-// 						header('location:home');
-
-// 					} else {
-						
-// 						++$intentos;
-// 						$datosController = array('usuarioActual'=>$usuarioActual, 
-// 												 'actualizarIntentos'=>$intentos);
-// 						$respuestaActualizarIntentos = LoginM::intentosModelo($datosController, 'usuarios');
-
-// 						echo '<div class="alert alert-danger" role="alert">Usuario o contraseña incorrectos</div>';
-// 					}
-					
-// 				} else {
-// 					$intentos = 0;
-// 					$datosController = array('usuarioActual'=>$usuarioActual, 
-// 											 'actualizarIntentos'=>$intentos);
-					
-// 					$respuestaActualizarIntentos = LoginM::intentosModelo($datosController, 'usuarios');
-// 					echo '<div class="alert alert-danger" role="alert">Ha ingresado 3 veces incorrectamente su contraseña, por favor, utilice el captcha.</div>';
-// 				}
-// 			}
-// 		}
-// 	}
-// }

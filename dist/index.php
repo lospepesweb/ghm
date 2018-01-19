77a74c93cb39b7e0df98191f31ce5c42f7526dc5
Petro232 <andpetronio@outlook.com>
2018-01-19 15:38:23 -0300

    cambio de rutas en index.php

--- dist/index.php
+++ dist/index.php
@@ -2,7 +2,7 @@
 
 $idioma = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);
 	if($idioma=="es") { 
-			header('Location: es'); 
+			header('Location: https://www.ghmtv.com/es'); 
 	} else { 
-			header( 'Location: en' ); 
+			header( 'Location: https://www.ghmtv.com/en' ); 
 	}
